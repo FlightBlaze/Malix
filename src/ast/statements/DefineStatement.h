e12bc43905edbe36a8db7edb139461b1d9bd8c5f
Edward Gigolaev <49155506+Alkanores@users.noreply.github.com>
2019-05-27 21:59:31 +0300

    Fix Build For Windows

--- src/ast/statements/DefineStatement.h
+++ src/ast/statements/DefineStatement.h
@@ -8,9 +8,10 @@
 class DefineStatement : public Statement {
 public:
     explicit DefineStatement(std::string name, Expression * expression)
-        : name(std::move(name)), expression(expression) {}
-    ~DefineStatement() override;
-    void execute() override;
+        : name(std::move(name)), expression(expression) {};
+
+    ~DefineStatement() override {};
+    void execute() override {};
 private:
     std::string name;
     Expression * expression;
