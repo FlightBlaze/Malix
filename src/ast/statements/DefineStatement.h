2358a7e4366f82d3afb2f224de1207b2ff54f4f1
Edward Gigolaev <49155506+Alkanores@users.noreply.github.com>
2019-05-27 23:41:24 +0300

    Added 8 functions in math package

--- src/ast/statements/DefineStatement.h
+++ src/ast/statements/DefineStatement.h
@@ -8,10 +8,9 @@
 class DefineStatement : public Statement {
 public:
     explicit DefineStatement(std::string name, Expression * expression)
-        : name(std::move(name)), expression(expression) {};
-
-    ~DefineStatement() override {};
-    void execute() override {};
+            : name(std::move(name)), expression(expression) {}
+    ~DefineStatement() override;
+    void execute() override;
 private:
     std::string name;
     Expression * expression;
